9331c0337919c3e9eadc9926584af6622eb03d0a
Gabriela Lima <lucaslopesllima@gmail.com>
2023-04-10 15:12:32 -0300

    Create Schedule Service and controller

--- src/main/java/br/com/feltex/whatsapbot/Controller/ScheduleController.java
+++ src/main/java/br/com/feltex/whatsapbot/Controller/ScheduleController.java
@@ -1,10 +1,19 @@
 package br.com.feltex.whatsapbot.Controller;
 
+import br.com.feltex.whatsapbot.Model.Schedule;
+import br.com.feltex.whatsapbot.ServiceImpl.ScheduleServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/scheule")
+@RequestMapping("/schedule")
 public class ScheduleController {
 
+    private ScheduleServiceImpl scheduleService;
+
+    @PostMapping("/save")
+    public Schedule save(Schedule schedule){
+        return scheduleService.save(schedule);
+    }
 }
--- src/main/java/br/com/feltex/whatsapbot/ServiceImpl/ScheduleServiceImpl.java
+++ src/main/java/br/com/feltex/whatsapbot/ServiceImpl/ScheduleServiceImpl.java
@@ -1,11 +1,18 @@
 package br.com.feltex.whatsapbot.ServiceImpl;
 
 import br.com.feltex.whatsapbot.Model.Schedule;
+import br.com.feltex.whatsapbot.Repository.ScheduleRepository;
 import br.com.feltex.whatsapbot.Service.ScheduleService;
 
 public class ScheduleServiceImpl implements ScheduleService {
+    private ScheduleRepository scheduleRepository;
+
+    public ScheduleServiceImpl(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
     @Override
     public Schedule save(Schedule schedule) {
-        return null;
+        return scheduleRepository.save(schedule);
     }
 }
